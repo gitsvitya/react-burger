4fdda22eccfc15bd8d4cc121df959722ecafe43c
Виктор Строков <strokov.victor@gmail.com>
2022-12-05 02:55:07 +0300

    Added BurgerIngredients section (without css)

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import appstyles from './App.module.css';
-import AppHeader from "../AppHeader/AppHeader.jsx";
+import appStyles from './App.module.css';
+import AppHeader from '../AppHeader/AppHeader.jsx';
+import BurgerIngredients from '../BurgerIngredients/BurgerIngredients.jsx';
 
 function App() {
   return (
-    <div className={appstyles.page}>
+    <div className={appStyles.page}>
       <AppHeader/>
+      <main className={appStyles.main}>
+        <BurgerIngredients/>
+      </main>
     </div>
   );
 }
