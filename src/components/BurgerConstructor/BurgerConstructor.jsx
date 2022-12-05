4c582701932d107ca74ac71b779d0649bc96147f
Виктор Строков <strokov.victor@gmail.com>
2022-12-05 05:35:33 +0300

    Added BurgerConstructor and BurgerIngredients with CSS

--- src/components/BurgerConstructor/BurgerConstructor.jsx
+++ src/components/BurgerConstructor/BurgerConstructor.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import bcStyles from './BurgerConstructor.module.css';
-import { ConstructorElement, Button, DragIcon, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import {ConstructorElement, Button, DragIcon, CurrencyIcon} from '@ya.praktikum/react-developer-burger-ui-components';
 import data from '../../utils/data.js';
 import listBorderPic from '../../images/illustration.svg'
 
@@ -10,52 +10,48 @@ function BurgerConstructor() {
       <ul className={bcStyles.list}>
         <li className={bcStyles.listBorder}>
           <ConstructorElement
-            text = 'Краторная булка N-200i (верх)'
-            thumbnail = { listBorderPic }
-            price = {20}
-            type = 'top'
-            isLocked = {true}
+            text='Краторная булка N-200i (верх)'
+            thumbnail={listBorderPic}
+            price={20}
+            type='top'
+            isLocked={true}
           />
         </li>
 
-          {
-            data.map((ingredient) => {
-              if(ingredient.type !== 'bun') {
-                return (
-                  <li className = { bcStyles.item } key = { ingredient._id }>
-                    <div className = { bcStyles.dragIcon }>
-                      <DragIcon type = 'primary' />
-                    </div>
-                    <ConstructorElement
-                      text = {ingredient.name}
-                      thumbnail = {ingredient.image_mobile}
-                      price = {ingredient.price}
-                    />
-                  </li>
-                )
-              }
-            })
-          }
+        {
+          data.map((ingredient) => {
+            if (ingredient.type !== 'bun') {
+              return (
+                <li className={bcStyles.listItem} key={ingredient._id}>
+                  <DragIcon type='primary'/>
+                  <ConstructorElement
+                    text={ingredient.name}
+                    thumbnail={ingredient.image_mobile}
+                    price={ingredient.price}
+                  />
+                </li>
+              )
+            }
+          })
+        }
 
-        <li className = {bcStyles.listBorder}>
+        <li className={bcStyles.listBorder}>
           <ConstructorElement
-            text = 'Краторная булка N-200i (низ)'
-            thumbnail = { listBorderPic }
-            price = {20}
-            type = 'bottom'
-            isLocked = {true}
+            text='Краторная булка N-200i (низ)'
+            thumbnail={listBorderPic}
+            price={20}
+            type='bottom'
+            isLocked={true}
           />
         </li>
       </ul>
 
-      <div className = {bcStyles.orderResultContainer}>
-        <div className = {bcStyles.priceSum}>
-          <p className = 'text text_type_digits-medium'>610</p>
-          <div className = {bcStyles.currencyIcon}>
-            <CurrencyIcon type = 'primary'/>
-          </div>
+      <div className={bcStyles.orderResultContainer}>
+        <div className={bcStyles.priceSum}>
+          <p className='text text_type_digits-medium'>610</p>
+          <CurrencyIcon type='primary'/>
         </div>
-        <Button type = 'primary' size = 'large' htmlType = 'button'>Оформить заказ</Button>
+        <Button type='primary' size='large' htmlType='button'>Оформить заказ</Button>
       </div>
     </section>
   )
--- src/components/BurgerIngredients/BurgerIngredients.jsx
+++ src/components/BurgerIngredients/BurgerIngredients.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import biStyles from './BurgerIngredients.module.css';
-import {Tab , Counter , CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import {Tab, Counter, CurrencyIcon} from '@ya.praktikum/react-developer-burger-ui-components';
 import data from '../../utils/data.js';
 
 function BurgerIngredients() {
@@ -9,76 +9,78 @@ function BurgerIngredients() {
 
   const handleScroll = (id) => {
     setCurrent(id);
-    document.querySelector(`#${id}`)?.scrollIntoView({ behavior: "smooth" })
+    document.querySelector(`#${id}`)?.scrollIntoView({behavior: "smooth"})
   }
 
   return (
     <section className={biStyles.section}>
-    <h1 className="text text_type_main-large">Соберите бургер</h1>
+      <h1 className="text text_type_main-large">Соберите бургер</h1>
       <div className={biStyles.tabHolder}>
         <Tab value={'one'} active={current === 'one'} onClick={handleScroll}>Булки</Tab>
         <Tab value={'two'} active={current === 'two'} onClick={handleScroll}>Соусы</Tab>
         <Tab value={'three'} active={current === 'three'} onClick={handleScroll}>Начинки</Tab>
       </div>
       <div className={biStyles.ingredients}>
-        <h2 className='text text_type_main-medium' id='one'>Булки</h2>
-        <ul className={biStyles.list}>
-          {data.map((ingredient) => {
-            if(ingredient.type === 'bun') {
-              return (
-                <li className={biStyles.listItem} key={ingredient._id}>
-                  <img src={ingredient.image} alt={ingredient.name}/>
-                  <Counter count={1} size='default' extraClass={biStyles.counter}/>
-                  <div className={biStyles.priceContainer}>
-                    <p className='text text_type_digits-default'>{ingredient.price}</p>
-                    <CurrencyIcon type='primary'/>
-                  </div>
-                  <p className='text text_type_main-default'>{ingredient.name}</p>
-                </li>
-              )
-            }
-          })}
-        </ul>
-      </div>
-      <div className={biStyles.ingredients}>
-        <h2 className='text text_type_main-medium' id='two'>Соусы</h2>
-        <ul className={biStyles.list}>
-          {data.map((ingredient) => {
-            if(ingredient.type === 'sauce') {
-              return (
-                <li className={biStyles.listItem} key={ingredient._id}>
-                  <img src={ingredient.image} alt={ingredient.name}/>
-                  <Counter count={1} size='default' extraClass={biStyles.counter}/>
-                  <div className={biStyles.priceContainer}>
-                    <p className='text text_type_digits-default'>{ingredient.price}</p>
-                    <CurrencyIcon type='primary'/>
-                  </div>
-                  <p className='text text_type_main-default'>{ingredient.name}</p>
-                </li>
-              )
-            }
-          })}
-        </ul>
-      </div>
-      <div className={biStyles.ingredients}>
-        <h2 className='text text_type_main-medium' id='three'>Начинки</h2>
-        <ul className={biStyles.list}>
-          {data.map((ingredient) => {
-            if(ingredient.type === 'main') {
-              return (
-                <li className={biStyles.listItem} key={ingredient._id}>
-                  <img src={ingredient.image} alt={ingredient.name}/>
-                  <Counter count={1} size='default' extraClass={biStyles.counter}/>
-                  <div className={biStyles.priceContainer}>
-                    <p className='text text_type_digits-default'>{ingredient.price}</p>
-                    <CurrencyIcon type='primary'/>
-                  </div>
-                  <p className='text text_type_main-default'>{ingredient.name}</p>
-                </li>
-              )
-            }
-          })}
-        </ul>
+        <div className={biStyles.ingredientsParts}>
+          <h2 className='text text_type_main-medium' id='one'>Булки</h2>
+          <ul className={biStyles.list}>
+            {data.map((ingredient) => {
+              if (ingredient.type === 'bun') {
+                return (
+                  <li className={biStyles.listItem} key={ingredient._id}>
+                    <img src={ingredient.image} alt={ingredient.name}/>
+                    <Counter count={1} size='default' extraClass={biStyles.counter}/>
+                    <div className={biStyles.priceContainer}>
+                      <p className='text text_type_digits-default'>{ingredient.price}</p>
+                      <CurrencyIcon type='primary'/>
+                    </div>
+                    <p className='text text_type_main-default'>{ingredient.name}</p>
+                  </li>
+                )
+              }
+            })}
+          </ul>
+        </div>
+        <div className={biStyles.ingredientsParts}>
+          <h2 className='text text_type_main-medium' id='two'>Соусы</h2>
+          <ul className={biStyles.list}>
+            {data.map((ingredient) => {
+              if (ingredient.type === 'sauce') {
+                return (
+                  <li className={biStyles.listItem} key={ingredient._id}>
+                    <img src={ingredient.image} alt={ingredient.name}/>
+                    <Counter count={1} size='default' extraClass={biStyles.counter}/>
+                    <div className={biStyles.priceContainer}>
+                      <p className='text text_type_digits-default'>{ingredient.price}</p>
+                      <CurrencyIcon type='primary'/>
+                    </div>
+                    <p className='text text_type_main-default'>{ingredient.name}</p>
+                  </li>
+                )
+              }
+            })}
+          </ul>
+        </div>
+        <div className={biStyles.ingredientsParts}>
+          <h2 className='text text_type_main-medium' id='three'>Начинки</h2>
+          <ul className={biStyles.list}>
+            {data.map((ingredient) => {
+              if (ingredient.type === 'main') {
+                return (
+                  <li className={biStyles.listItem} key={ingredient._id}>
+                    <img src={ingredient.image} alt={ingredient.name}/>
+                    <Counter count={1} size='default' extraClass={biStyles.counter}/>
+                    <div className={biStyles.priceContainer}>
+                      <p className='text text_type_digits-default'>{ingredient.price}</p>
+                      <CurrencyIcon type='primary'/>
+                    </div>
+                    <p className='text text_type_main-default'>{ingredient.name}</p>
+                  </li>
+                )
+              }
+            })}
+          </ul>
+        </div>
       </div>
     </section>
   )
